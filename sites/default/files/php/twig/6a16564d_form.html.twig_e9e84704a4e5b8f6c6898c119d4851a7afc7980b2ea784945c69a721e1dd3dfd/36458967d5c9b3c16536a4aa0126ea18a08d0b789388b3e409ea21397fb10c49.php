3ce9e6d519439f3bfaa5e498be52ac1b5ec376d2
nehajyoti <nehajyoti04@gmail.com>
2017-03-22 16:48:57 +0530

    Task 7

---
+++ sites/default/files/php/twig/6a16564d_form.html.twig_e9e84704a4e5b8f6c6898c119d4851a7afc7980b2ea784945c69a721e1dd3dfd/36458967d5c9b3c16536a4aa0126ea18a08d0b789388b3e409ea21397fb10c49.php
@@ -0,0 +1,84 @@
+<?php
+
+/* core/themes/classy/templates/form/form.html.twig */
+class __TwigTemplate_724dc527e16eb2d021e4d11ca730b3c0b11fec616d6b5ca90667d08140742b63 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<form";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">
+  ";
+        // line 14
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true));
+        echo "
+</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  48 => 14,  43 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a 'form' element.*/
+/*  **/
+/*  * Available variables*/
+/*  * - attributes: A list of HTML attributes for the wrapper element.*/
+/*  * - children: The child elements of the form.*/
+/*  **/
+/*  * @see template_preprocess_form()*/
+/*  *//* */
+/* #}*/
+/* <form{{ attributes }}>*/
+/*   {{ children }}*/
+/* </form>*/
+/* */
---
+++ sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'D8-activities',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = '1mgJMHx9fc6zW0HLVhPff89jN2nDTzzHUsdt3nYjzo5FvgJRMkdhslD-B7dZZCMr8ZOEC-l3ZA';
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config/sync';
